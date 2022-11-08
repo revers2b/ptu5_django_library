ad2c33cf1fa933be2f8933d9a23addf0dab03684
Inaros <vkevinas99@gmail.com>
2022-11-08 08:19:07 +0200

    =

--- ptu5_library/ptu5_library/settings.py
+++ ptu5_library/ptu5_library/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(qsw3mc_a_=%jrhln1xhts)$nk&=#y(^4j86^!^wz6qk#rce##'
+SECRET_KEY = local_settings.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
