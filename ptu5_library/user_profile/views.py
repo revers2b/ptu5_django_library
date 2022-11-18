8c582eb47d79822e76e69ab6979535abcca49c42
Inaros <vkevinas99@gmail.com>
2022-11-18 14:49:24 +0200

    profile picture

--- ptu5_library/user_profile/views.py
+++ ptu5_library/user_profile/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_protect
 from django.contrib import messages
 from django.core.validators import validate_email
@@ -35,3 +36,7 @@ def register (request):
             return redirect('login')
 
     return render(request, 'user_profile/register.html')
+
+@login_required
+def profile(request):
+    return render(request, 'user_profile/profile.html')
